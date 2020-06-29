228e9f2206a36175a09e6883b7ee81d130c74b64
saltyJeff <saltyJeff@users.noreply.github.com>
2020-06-29 00:48:54 -0700

    touched up the main.cpp documentation

--- example/main.cpp
+++ example/main.cpp
@@ -20,7 +20,7 @@ map<uint16_t, uint16_t> valueMap;
  * The Modbus server (slave) interacts with your code through the use of callbacks.
  *
  * For example, getShort(addr) will be called to return the value of the register at addr, where addr
- * will be 30001-39999 for a input register and 40001-49999
+ * will be 30001-39999 for a input register and 40001-49999 for a holding register
  *
  * Likewise, setShort(addr, value) will be called to set the value of the register at addr to value,
  * where addr will be between 40001-49999
@@ -36,9 +36,6 @@ uint16_t getShort(uint16_t addr) {
 	}
 	return addr;
 }
-bool getBool(uint16_t addr) {
-	return addr % 2 == 0;
-}
 void setShort(uint16_t addr, uint16_t value) {
 	valueMap[addr] = value;
 }
@@ -47,19 +44,21 @@ void setShort(uint16_t addr, uint16_t value) {
  * Accessors is just a quick way to bundle together get/set Bool and get/set Short
  * The default setter is a no-op, and the default getter returns 0/false
  */
-Accessors accs { .getBool = getBool, .getShort = getShort, .setShort = setShort };
+Accessors accs { .getShort = getShort, .setShort = setShort };
 
-/*
- * We specify a callback for the server to call with a Response object
- * The response object is how we can send a response back to the modbus master.
- * There are different ways to respond, here we just respond with an accessors object
+/* This pattern is inspired by the express/hapi web servers in NodeJS.
  *
- * The library will automagically fill out a proper modbus response by calling the getters/setters
- * until it has all the values it needs.
+ * We specify a callback for the server to call with a Response object once a complete command frame is parsed.
+ * Through the response object we can send a response back to the modbus master.
+ *
+ * There are different ways to respond, here we just respond with an accessors object.
+ * The library will automagically fill out a proper modbus response and send it to the client
+ * by calling the getters/setters within
+ * the accessor until it has all the values it needs.
  *
  * If you need a custom response (or don't trust my code :( ) you can analyze the modbus command
  * frame received from the server using res.frame (here we use it to just print out the details
- * of an incoming command)
+ * of an incoming command). Then you can respond by using res.respRaw
  */
 void mbCallback(Responder res) {
 	cout << res.frame.str() << endl;
@@ -70,6 +69,7 @@ int main (int argc, char *argv[]) {
 	// to initialize the server, provide a port and a modbus callback.
 	// RAII means the server will automatically close its internal ports on deallocation
 	ModbusTCPServer server(5020, mbCallback);
+	// to be compatible with what ever event loop you prefer, the server only checks its epoll when you call server.tick()
 	while(true) {
 		server.tick();
 	}
